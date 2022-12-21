9bacb4dfff98c9644c150020f74daac37be60d33
Deveesh Shetty <deveeshshetty@gmail.com>
2022-12-21 20:34:15 +0530

    Added links to gihub in project

--- src/components/Projects/Card.jsx
+++ src/components/Projects/Card.jsx
@@ -1,15 +1,17 @@
 import React from "react"
 
-function Card({ title, img, tech, description }) {
+function Card({ title, img, tech, description, link }) {
   return (
-    <div className="flex flex-col gap-2 border-2 mx-6 lg:px-2 md:items-center py-2 my-4 -sm:min-w-[75%] cursor-pointer rounded-xl">
+    <div className="flex flex-col gap-2 border-2 mx-6 lg:px-2 md:items-center py-2 my-4 -sm:min-w-[75%] rounded-xl">
       <div className="mb-4 rounded-t-3xl">
-        <div className="rounded-t-3xl">
-          <img
-            src={img}
-            alt={title}
-            className="aspect-video p-4 min-w-[20rem] lg:min-w-[24rem] -md:min-w-[75%] rounded-3xl mx-auto pointer-events-none"
-          />
+        <div className="rounded-t-3xl cursor-pointer">
+          <a href={link} target="_blank">
+            <img
+              src={img}
+              alt={title}
+              className="aspect-video p-4 min-w-[20rem] lg:min-w-[24rem] -md:min-w-[75%] rounded-3xl mx-auto pointer-events-none"
+            />
+          </a>
         </div>
         <p className="text-center text-2xl font-semibold font-playfair">
           {title}
--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -7,7 +7,10 @@ import Card from "./Card"
 function Project() {
   const project = useRef(null)
   const scroll = (scrollOffset) => {
+    console.log(project.current.scrollLeft)
+    if (!project.current.scrollLeft) project.current.scrollLeft = 0
     project.current.scrollLeft += scrollOffset
+    console.log(scrollOffset)
   }
   return (
     <div id="projects">
@@ -17,11 +20,11 @@ function Project() {
       />
       <div className="relative select-none">
         <div
-          className="absolute top-1/2 -translate-y-1/2 z-10 right-2 rounded-full p-1 bg-accent opacity-80 animate-bounce-horizontal md:p-2 cursor-pointer scroll-smooth"
-          // onClick={() => {
-          //   if (window.innerWidth <= 768) scroll(window.innerWidth - 100)
-          //   else scroll(window.innerWidth / 3)
-          // }}
+          className="md:hidden lg:block absolute top-1/2 -translate-y-1/2 z-10 right-2 rounded-full p-1 bg-accent opacity-80 animate-bounce-horizontal md:p-2 cursor-pointer scroll-smooth"
+          onClick={() => {
+            if (window.innerWidth <= 768) scroll(window.innerWidth - 100)
+            else scroll(window.innerWidth / 3)
+          }}
         >
           <FaAngleRight />
         </div>
@@ -35,6 +38,7 @@ function Project() {
           <FaAngleLeft />
         </div> */}
         <ScrollContainer
+          component="div"
           className="grid grid-cols-2 lg:grid-cols-3 cursor-grab lg:flex lg:overflow-x-scroll no-scroll -sm:flex -sm:overflow-x-scroll mb-6"
           ref={project}
         >
@@ -43,12 +47,14 @@ function Project() {
             img="Projects/ExpenseTracker.png"
             tech={["MongoDB", "ExpressJS", "ReactJs", "NodeJS", "TailwindCSS"]}
             description="In this Expense tracker user can add his expenses and view all the expenses till date. Also on top of that there is a stocks section where user can keep track of how much they are holding in stocks."
+            link="https://github.com/Deveesh-Shetty/Expense-Tracker"
           />
           <Card
             title="Clouds"
             img="Projects/Clouds.png"
             tech={["ReactJs", "CSS3"]}
             description="CLOUDS is an association of Computer Science Department in Sahyadri College of Engineering and Management. This website shows the details of office bearers representing clouds. The event tab it shows the upcoming events in our college"
+            link="https://github.com/Deveesh-Shetty/Clouds"
           />
           <Card
             title="Project Phase"
@@ -73,6 +79,7 @@ function Project() {
             img="Projects/ApiForever.png"
             tech={["ReactJs", "CSS3", "Bunch of APIs"]}
             description="API Forever formerly known as API Fantasy was one of my first React Project. Here I used a bunch of public APIs as the name suggests. It uses Joke, Quotes, ISRO, Dictionary and Pincode API."
+            link="https://github.com/Deveesh-Shetty/API-Forever"
           />
         </ScrollContainer>
       </div>
