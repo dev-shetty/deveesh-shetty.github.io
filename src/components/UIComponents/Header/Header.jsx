6578af83afaab468507c05e14bb075858464aeb4
Deveesh Shetty <deveeshshetty@gmail.com>
2022-12-24 19:36:26 +0530

    Added what am i doing now section

--- src/components/UIComponents/Header/Header.jsx
+++ src/components/UIComponents/Header/Header.jsx
@@ -2,7 +2,7 @@ function Header({ heading, subHeading }) {
   return (
     <header className="font-playfair text-center mb-8">
       <div className="flex justify-center">
-        <p className="text-4xl mb-2 font-bold pt-4 text-gradient w-fit">
+        <p className="text-4xl mb-2 font-bold pt-4 text-gradient w-fit py-2">
           {heading}
         </p>
       </div>
